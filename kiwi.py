3b2a5efdb74123129da81a4b91b8965b8aaafeff
Ubuntu <ubuntu@ip-10-122-215-165.ec2.internal>
2011-11-13 03:33:58 +0000

    ServerSide JSON fix

--- kiwi.py
+++ kiwi.py
@@ -5,14 +5,13 @@ import nltk
 from nltk import *
 from nltk.corpus import PlaintextCorpusReader
 from nltk.stem import *
+import json
 
 def make_text(string):
 	return string
 
 
-urls = (
-	'/', 'hello',
-	'/rev', 'firstrev')
+urls = ('/', 'hello')
 
 app = web.application(urls, globals())
 render = web.template.render('templates/')
@@ -20,16 +19,6 @@ render = web.template.render('templates/')
 myform = web.form.Form(
                 web.form.Textarea('content', rows=40, cols=80),
                 )
-
-
-class firstrev:
-	def POST(self):
-		i =  web.input()
-		text = i.text
-		tokens = WordPunctTokenizer().tokenize(text)
-		stemmer = LancasterStemmer()	
-		txt = stemmer.stem(tokens[0])
-		return txt		
 
 class hello: 
 	def GET(self):
@@ -45,74 +34,57 @@ class hello:
 		finder = ParseBigramCollocationsAndWords(tokens)
 		repetitions = DetectRepetitions(finder, tokens)
 		stemmer = LancasterStemmer()
-		#return make_text(content)
-		#return repetitions
-		returnhtml = """<head>
-<title>Your writing is improving!</title>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-<script type="text/javascript" src="/static/jquery.editable-1.3.3.min.js"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-
-$("span").css({'font-weight':'bold'});
-
-var enter = function()
-                {
-			var classname = this.className
-			$("." + classname).css({color:'red'});
-                }
-
-var leave = function()
-		{
-			var classname = this.className
-			$("." + classname).css({color:'black'});
-		}
-
-var end = function(content)
-		{
-			console.log($(this).get())
-			var self = this
-			$.post("/rev", {text : content.current}, function(data){
-				console.log(data)
-				console.log($(self).get())
-				$(self).removeClass(self.className)
-				$(self).addClass(data)
-			});
-		}
-
-$("span").hover(enter, leave);
-
-$("span").editable({onSubmit:end});
-
-});
-
-
-</script>
-
-</head>
-<body>
-<h1>Bolded Possible Repetitions</h1>
-<p>"""
+		wrds = []
 		index = 0
 		for token in tokens:
 			if (token == '"'):
 				returnhtml = returnhtml + "\"" +  " " #a hack, fix this on serverside
-			#elif (token[:4] == """\r\n"""):
-			#	print "detected"
-			#	returnhtml = returnhtml + """</p> <p>"""
+				makeJson = {}
+                                makeJson['word'] = "\""
+                                makeJson['properties'] = {}
+                                makeJson['properties']['stem'] = stemmer.stem(token)
+                                makeJson['properties']['repetitions'] = False
+				makeJson['specialChar'] = False
+                                wrds.append(makeJson)
 			elif ("""\r\n""" in token):
-				print "detected"
-				returnhtml = returnhtml + """</p> <p>"""
+				makeJson = {}
+                                makeJson['word'] = "paragraphbreak"
+                                makeJson['properties'] = {}
+                                makeJson['properties']['stem'] = stemmer.stem(token)
+                                makeJson['properties']['repetitions'] = False
+				makeJson['specialChar'] = True
+                                wrds.append(makeJson)
 			elif index in repetitions:
-				stemmedtoken = stemmer.stem(token)
-				returnhtml = returnhtml +("""<span id="repetition" class="%s"> %s </span>""" %(stemmedtoken , token))
+				makeJson = {}
+                                makeJson['word'] = token
+                                makeJson['properties'] = {}
+                                makeJson['properties']['stem'] = stemmer.stem(token)
+                                makeJson['properties']['repetitions'] = True
+				makeJson['specialChar'] = False
+                                wrds.append(makeJson)
+
+			elif (token == " "):
+				makeJson = {}
+                                makeJson['word'] = "space"
+                                makeJson['properties'] = {}
+                                makeJson['properties']['stem'] = stemmer.stem(token)
+                                makeJson['properties']['repetitions'] = False
+				makeJson['specialChar'] = True
+                                wrds.append(makeJson)
 			else: 
-				returnhtml = returnhtml + token + " " 
+				#returnhtml = returnhtml + token + " " 
+				makeJson = {}
+				makeJson['word'] = token
+				makeJson['properties'] = {}
+				makeJson['properties']['stem'] = stemmer.stem(token)
+				makeJson['properties']['repetitions'] = False
+				makeJson['specialChar'] = False
+				wrds.append(makeJson)
+
 			index = index + 1
-		
-		return returnhtml + """</p>
-</body>
-</html>"""
+		returnJson = json.dumps(wrds)
+		return returnJson
+				
 
 class WordPunctSpaceTokenizer(RegexpTokenizer):
 	def __init__(self):
@@ -194,31 +166,6 @@ def DetectRepetitions(finder, tokens):
 							indexesofrepetitions = indexesofrepetitions + [phraseindexes[i]]
                                         i = i + 1
 	return indexesofrepetitions			
-'''
-def main(argv):
-	corpus_root = './'
-	wordlists = PlaintextCorpusReader(corpus_root, '.*')
-	text = Text(wordlists.words(argv))
-	finder = ParseBigramCollocationsAndWords(text.tokens)
-	bigram_measures = BigramAssocMeasures() 
-	collocations = finder.nbest(bigram_measures.likelihood_ratio, 20)
-	colloc_strings = [w1+' '+w2 for w1, w2 in collocations]
-	print colloc_strings
-	stemmer = LancasterStemmer()
-	repetitions = detectrepetitions(finder, text)
-	print repetitions
-
-	for phrase in finder.StemmedPhrasesIndexes:
-		if  not phrase in ['the','s','.',';',',',"'",":", "`","(",")"] : 
-			phraseindexes = finder.StemmedPhrasesIndexes[phrase]
-			if len(phraseindexes) > 1:
-				i = 1
-				while i < len(phraseindexes):
-					diff = phraseindexes[i] - phraseindexes[i - 1]
-					if diff < 100:
-						print text.tokens[phraseindexes[i-1]] + " " +  text.tokens[phraseindexes[i]] + "\n"
-					i = i + 1	
-'''						
 if __name__ == "__main__":
 #	main(sys.argv[1])
 	app.run()
